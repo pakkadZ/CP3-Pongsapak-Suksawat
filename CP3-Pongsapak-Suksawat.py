c65e4def32c3cea1102e0dec519074c657393f3d
pakkadZ <pakkadjor00@gmail.com>
2022-12-17 22:31:16 +0700

    finish

--- CP3-Pongsapak-Suksawat.py
+++ CP3-Pongsapak-Suksawat.py
@@ -1,2 +1,13 @@
-print("Hello py")
-print
+eng = 80
+Bus = 70
+Genaral =66.45
+com = 88.95
+compro =77.69
+
+
+
+print("--- Your Score ---")
+print("Foundation English : ",eng)
+print("General Business : ",Genaral)
+print("Introduction to Computer Systems : ",com)
+print("Computer Programming : ",compro)
